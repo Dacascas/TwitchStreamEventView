04bac01d91bc07c7905161a66309da72c147139d
dacascas <x_casper@ukr.net>
2019-04-10 23:39:34 +0300

    update

--- src/config/config.php
+++ src/config/config.php
@@ -6,6 +6,6 @@ return [
     'twitch.options'         => [
         'secret'    => 'kznlnrrtld01u1ua33jbm6pcoiccf5',
         'client_id' => 'ugwrbsdba3kj9z8vu1jgviwkkfbwe1',
-        'redirect_uri' => 'http://localhost:8010/auth/redirect'
+        'redirect_uri' => 'https://tranquil-lowlands-72927.herokuapp.com/auth/redirect'
     ]
 ];
