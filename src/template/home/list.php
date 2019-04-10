fc4ac306311e603cd2d033b4a97569a692c25ab3
dacascas <x_casper@ukr.net>
2019-04-11 01:10:48 +0300

    update

--- src/template/home/list.php
+++ src/template/home/list.php
@@ -1,6 +1,6 @@
 <?php
 echo '<ul>';
 foreach ($list as $item) {
-    echo \sprintf("<li><a href=\"/stream/%s\">%s</li>", $item['channel']['_id'], $item['channel']['display_name']);
+    echo \sprintf("<li><a href=\"/streamer/%s\">%s</li>", $item['channel']['_id'], $item['channel']['display_name']);
 }
 echo '</ul>';
