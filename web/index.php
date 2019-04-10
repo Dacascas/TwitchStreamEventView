945f53b153ba2bd56b9227b1135d3475af67af57
dacascas <x_casper@ukr.net>
2019-04-10 12:33:10 +0300

    update

---
+++ web/index.php
@@ -0,0 +1,13 @@
+<?php
+
+var_dump($_SERVER);
+
+require '../vendor/autoload.php';
+
+$config = require_once '../src/config/config.php';
+$app = new \Slim\App(['settings' => $config]);
+
+require_once '../src/config/route.php';
+require_once '../src/config/di.php';
+
+$app->run();
