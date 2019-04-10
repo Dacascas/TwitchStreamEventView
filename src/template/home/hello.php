192352bc429a6ca6dc5addd39c1f8298196af09b
dacascas <x_casper@ukr.net>
2019-04-10 23:37:14 +0300

    update

--- src/template/home/hello.php
+++ src/template/home/hello.php
@@ -4,7 +4,7 @@ echo '<html>
         <body style="text-align:center;margin: 0 auto;display: block;">
             <button style="background-color: #6441a5;  padding:10px;">
                 <a href="' . $url . '" style="color:#fff; text-decoration: none">
-                <img width="20" src="web/twitch.png"> Login with Twitch
+                <img width="20" src="twitch.png"> Login with Twitch
                 </a>
             </button>
         </body>
