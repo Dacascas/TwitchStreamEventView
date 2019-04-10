a916f53ff2868d02a077765997914287a110f9b7
dacascas <x_casper@ukr.net>
2019-04-10 23:48:53 +0300

    update

--- src/template/home/favorite.php
+++ src/template/home/favorite.php
@@ -1,9 +1,9 @@
 <?php
 echo '<html>
         <head></head>
-        <body style="text-align:center;margin: 0 auto;display: block;">
+        <body style="padding-left:40%;">
             <p>' . $message . '</p>
-            <button style="background-color: #6441a5;  padding:10px;">
+            <button style="background-color: #6441a5;  padding:10px; position: absolute; top: 50%;">
                 <form method="post">
                     <input type="text" name="streamer-name"/>
                     <input class="favorite styled" type="submit" name="fav" value="Add to favorites">
