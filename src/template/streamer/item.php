eda30c5a34835424347070d74f33e2ddc35712ab
dacascas <x_casper@ukr.net>
2019-04-11 12:36:13 +0300

    update

--- src/template/streamer/item.php
+++ src/template/streamer/item.php
@@ -4,6 +4,8 @@ echo <<<HERE
 <html>
   <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
     <!-- Load the Twitch embed script -->
     <script src="https://embed.twitch.tv/embed/v1.js"></script>
 
@@ -23,8 +25,6 @@ echo <<<HERE
         player.play();
       });
     </script>
-  </head>
-  <body>
     <header>
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
             <div class="container">
