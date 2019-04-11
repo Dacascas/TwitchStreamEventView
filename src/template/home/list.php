7759e35e51720cc4d884fccf9281a011f497a988
dacascas <x_casper@ukr.net>
2019-04-11 13:24:45 +0300

    update

--- src/template/home/list.php
+++ src/template/home/list.php
@@ -1,6 +1,6 @@
 <?php
 echo '<ul class="list-group list-group-flush">';
 foreach ($list as $item) {
-    echo \sprintf("<li class='list-group-item'><a style='color:#6441a5' href=\"/streamer/%s\">%s</li>", $item['channel']['_id'], $item['channel']['display_name']);
+    echo \sprintf("<li class='list-group-item'><a style='color:#6441a5' href=\"/streamer/%s\">%s</li>", $item['channel']['name'], $item['channel']['display_name']);
 }
 echo '</ul>';
