70e28e77aa2e2b299a6b46a147ee0723305d68cf
dacascas <x_casper@ukr.net>
2019-04-11 12:58:42 +0300

    update

--- src/template/home/favorite.php
+++ src/template/home/favorite.php
@@ -15,7 +15,8 @@ echo <<<HERE
     <section class="section section-shaped section-lg mt-10">
     <div class="container">
         <div class="row mb-3">
-           <div class="px-auto">
+           <div class="col-sm-3"></div>
+           <div class="col-sm-9">
             <p>$message</p>
             <button style="background-color: #6441a5;  padding:10px; position: absolute; top: 50%;">
                 <form method="post">
